093d9a685339070aa56c58ea07bc5c657f694b16
marm369 <126961765+marm369@users.noreply.github.com>
2025-01-22 23:21:40 +0100

    suppression dossier panier

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import "react-native-gesture-handler";
-import  FilterBar  from "./src/features/home/screens/home-components/FilterBar";
-import  HomeScreen  from "./src/features/home/screens/HomeScreen";
 import AppNavigation from "./src/features/navigation/AppNavigation";
-import  ChatScreen from "./src/features/home/screens/home-components/Chat";
 
 export default function App() {
-  return  <AppNavigation />;
+  return <AppNavigation />;
 }
--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,2 @@
-  const endpoint = "http://192.168.1.1:3000";
-  export { endpoint };
-
+const endpoint = "http://192.168.1.72:3000";
+export { endpoint };
